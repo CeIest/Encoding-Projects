18fd01e7394e07fedc3ee197ea053658f2b9589e
CeIest <45696416+CeIest@users.noreply.github.com>
2021-07-25 04:04:43 +0200

    Update AGGO scripts

--- Sword Art Online - OP & ED collection (BD)/SAO_AGGO_OP1.py
+++ Sword Art Online - OP & ED collection (BD)/SAO_AGGO_OP1.py
@@ -89,7 +89,7 @@ class Encoding:
         assert self.file.a_src
         assert self.file.a_enc_cut
 
-        v_encoder = X265Encoder('settings/x265_settings_S1')
+        v_encoder = X265Encoder('settings/x265_settings_AGGO')
 
         a_extracters = [
             BasicTool(
